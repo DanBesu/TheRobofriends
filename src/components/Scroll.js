9e0c69db3d9cc0f30dd97dd651757fe692e14c34
911_Besu_Dan <danbesu15@gmail.com>
2021-02-14 14:57:35 +0200

    Update Scroll.js

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Scroll = (props) => {
     // Scroll has to render what s inside (children) of it in index.js
     return (
-        <div style={{overflowY: 'scroll', border: '1px solid black', height: '1000px'}}>
+        <div style={{overflowY: 'scroll', height: '1000px'}}>
             {props.children}
         </div>
     )
